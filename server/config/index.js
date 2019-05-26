f791e0f26e8478a54f4f67dc9900d5905a95fb00
rimvydaszilinskas <rimvydas.zilinskas@yahoo.com>
2019-05-26 15:41:36 +0200

    unhide config

---
+++ server/config/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+    database: {
+        connection: {
+            host: 'remotemysql.com',
+            port: 3306,
+            dialect: 'mysql',
+            database: 'qjQQLY5v4q',
+            username: 'qjQQLY5v4q',
+            password: 'yQ233cMbyo',
+        },
+        forceSync: true,
+        sync: false
+    }
+}
