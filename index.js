ec79404d9cb9c9f0d379b7b2f26c26495d7274df
rimvydaszilinskas <rimvydas.zilinskas@yahoo.com>
2019-05-26 14:53:29 +0200

    added cors

--- index.js
+++ index.js
@@ -22,6 +22,13 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use('/', router(config));
 
 app.listen(process.env.PORT || 3000, (err) => {
